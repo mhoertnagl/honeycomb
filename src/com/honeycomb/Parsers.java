51627461b2405fb76989102e0b976efa3e0a54fe
Mathias Hörtnagl <m.hoertnagl@gmail.com>
2022-07-17 15:49:37 +0200

    seq parsers

--- src/com/honeycomb/Parsers.java
+++ src/com/honeycomb/Parsers.java
@@ -1,6 +1,7 @@
 package com.honeycomb;
 
 import com.honeycomb.parsers.*;
+import com.honeycomb.tuples.Tuples.*;
 
 import java.util.*;
 
@@ -26,6 +27,10 @@ public class Parsers {
         return parser.map(s -> null);
     }
 
+    public static <T1, T2> Seq2Parser<T1, T2> seq(Parser<T1> p1, Parser<T2> p2) {
+        return new Seq2Parser<>(p1, p2);
+    }
+
     @SafeVarargs
     public static <T> Parser<T[]> seq(Parser<T> parser, Parser<T>... parsers) {
         return new SeqParser<>(parser, parsers);
@@ -41,53 +46,24 @@ public class Parsers {
     }
 
     public static <T> Parser<List<T>> many1(Parser<T> parser) {
-        return seq(parser.map(Collections::singletonList), many(parser)).map(Parsers::flatten);
-    }
-
-    private static <T> List<T> flatten(Collection<T>[] lists) {
-        final var res = new ArrayList<T>();
-        for (final var list : lists) {
-            if (list != null) {
-                res.addAll(list);
-            }
-        }
-        return res;
-    }
-
-//  private static <T> List<T> flatten(Iterable<? extends Iterable<T>> lists) {
-//    final var res = new ArrayList<T>();
-//    for (final var list : lists) {
-//      if (list != null) {
-//        for (final var elem : list) {
-//          res.add(elem);
-//        }
-//      }
-//    }
-//    return res;
-//  }
-
-    private static <T> List<T> flatten(Iterable<T[]> lists) {
-        final var res = new ArrayList<T>();
-        for (final var list : lists) {
-            if (list != null) {
-                res.addAll(Arrays.stream(list).toList());
-            }
-        }
-        return res;
+        return seq(parser, many(parser)).map((v1, v2) -> { v2.add(0, v1); return v2; });
     }
 
     public static <T> Parser<Optional<T>> maybe(Parser<T> parser) {
         return new MaybeParser<>(parser);
     }
 
-    public static <T> Parser<Optional<List<T>>> list(String delim, Parser<T> parser) {
-        return list(literal(delim), parser);
+    public static <T> Parser<Optional<List<T>>> list(String delimiter, Parser<T> arg) {
+        return list(literal(delimiter), arg);
     }
 
-    public static <S, T> Parser<Optional<List<T>>> list(Parser<S> delim, Parser<T> parser) {
-        final var arg0 = parser.map(Collections::singletonList);
-        final var argv = many(seq(skip(delim), parser)).map(Parsers::flatten);
-        return maybe(seq(arg0, argv).map(Parsers::flatten));
+    public static <S, T> Parser<Optional<List<T>>> list(Parser<S> delimiter, Parser<T> arg) {
+        return maybe(seq(arg, many(seq(delimiter, arg)._2())).map(Parsers::prepend));
+    }
+
+    private static <T> List<T> prepend(T v, List<T> list) {
+        list.add(0, v);
+        return list;
     }
 }
 
