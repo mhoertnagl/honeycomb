da806dd689e9025f1f60df0bc291b20cb6038f05
Mathias Hörtnagl <m.hoertnagl@gmail.com>
2022-07-23 21:57:59 +0200

    cleanup

--- src/com/honeycomb/Parser.java
+++ src/com/honeycomb/Parser.java
@@ -15,12 +15,31 @@ import static com.honeycomb.Parsers.*;
  */
 public interface Parser<T> {
 
+    /**
+     * Holds the input string {@code in} and the current character position
+     * {@code pos} in the input string.
+     *
+     * @param in the input string to be parsed
+     * @param pos the character position in the input string
+     */
     record Cursor(String in, Integer pos) {
 
+        /**
+         * Advances the {@link Cursor} position by {@code offset} bytes.
+         *
+         * @param offset the number of bytes to advance the cursor's position
+         * @return a new {@link Cursor} offset by specified number of bytes.
+         */
         public Cursor advanceBy(Integer offset) {
             return new Cursor(in, pos + offset);
         }
 
+        /**
+         * Positions the {@link Cursor} at the new position {@code newPos}.
+         *
+         * @param newPos the new position of the cursor
+         * @return a new {@link Cursor} at the new position
+         */
         public Cursor positionAt(Integer newPos) {
             return new Cursor(in, newPos);
         }
@@ -37,6 +56,14 @@ public interface Parser<T> {
         }
     }
 
+    /**
+     * Parses the input at the current {@link Cursor} position and yields the
+     * new {@link State} containing the new {@link Cursor} position and the
+     * parsing result.
+     *
+     * @param cur the {@link Cursor} position before this parser is invoked
+     * @return the {@link State} before this parser is invoked
+     */
     Optional<State<T>> parse(Cursor cur);
 
     default Optional<T> parse(String in) {
